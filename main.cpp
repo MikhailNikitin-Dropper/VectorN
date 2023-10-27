4baf6c619787f1703858af256b635c47c9f6e1de
DropperMan <mike20010321@gmail.com>
2023-10-27 10:33:25 +0300

    Tests added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "Vector.h"
 int main() {
-    Vector<int, 5> v1;
-    Vector<float, 7> v2;
+    Vector<int, 4> v1;
+    Vector<float, 4> v2;
     v1.testVector();
     v2.testVector();
 }
